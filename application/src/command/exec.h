8490b0dfd4bc613b731a6ff619f363feb1500257
Álvaro Cebrián <alvaro.cebrian.garcia@gmail.com>
2019-12-31 10:54:51 +0100

    [PIT-7] Remove legacy code

--- application/src/command/exec.h
+++
@@ -1,7 +0,0 @@
-#ifndef _EXEC_H_
-#define _EXEC_H_
-
-int exec_cmd(int argc, char **argv);
-int exec_init();
-
-#endif
--- application/src/command/path.h
+++
@@ -1,7 +0,0 @@
-#ifndef _PATH_H_
-#define _PATH_H_
-
-int path_cmd(int argc, char **argv);
-int path_init(void);
-
-#endif
